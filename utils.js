5d7ce33069ad883c323ed33ccf3a2336fedaa853
uxeeCoder <umar.afar@aol.com>
2024-04-15 22:43:24 -0400

    created utils.js file for function invocation

---
+++ utils.js
@@ -0,0 +1,16 @@
+const { sum, multiply, isPrime } = require("./app");
+
+const results = sum(8, 10);
+const results2 = multiply(7, 12);
+const results5 = multiply(5, 0);
+const results6 = multiply(2, -5);
+const results3= isPrime(multiply (1, 7));
+const results4= isPrime(multiply (3, 2));
+console.log(results);
+console.log(results2);
+console.log(results3);
+console.log(results4);
+console.log(results5);
+console.log(results6);
+
+
